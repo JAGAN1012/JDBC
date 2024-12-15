a1582485df28cb9db49459958205bb956f741e01
JAGAN T M <100745418+JAGAN1012@users.noreply.github.com>
2024-12-15 21:46:42 +0530

    Update first.java

--- Javamongo/src/main/java/mongojava/first.java
+++ Javamongo/src/main/java/mongojava/first.java
@@ -1,5 +1,7 @@
 package mongojava;
 
+import java.util.*;
+
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCursor;
@@ -8,8 +10,9 @@ import com.mongodb.client.MongoDatabase;
 public class first {
 
     public static void main(String[] args) {
+    	 Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to MongoDB!");
-
+     
         
         MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
         System.out.println("MongoDB client created successfully.");
@@ -20,10 +23,19 @@ public class first {
 
      
         System.out.println("Listing all databases:");
-       MongoCursor<String> dbCursor = mongoClient.listDatabaseNames().iterator()
+       MongoCursor<String> dbCursor = mongoClient.listDatabaseNames().iterator();
             while (dbCursor.hasNext()) {
                 System.out.println(dbCursor.next());
             }
+            System.out.println("Enter the database name to view its collections: ");
+          
+			
+            String dbName = sc.next();
+            System.out.println("The collection in " +dbName+ " database");
+            db=mongoClient.getDatabase(dbName);
+            for (String collectionName : db.listCollectionNames()) {
+                System.out.println(collectionName);
+            }
         } 
     }
-}
+
